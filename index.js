307b08e3fe1da96dbef703b10c9e295c6aeae410
Andrew Besedin <andrew.besedin@intexsoft.by>
2025-08-03 21:11:53 +0300

    add warmup & error display

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function qsort(array) {
         var start = stackTop.start;
         var end = stackTop.end;
 
-        if (start >= end) continue;
+        // if (start >= end) continue;
 
         var pivotId = end;
         var pivot = array[pivotId];
@@ -54,6 +54,9 @@ function qsort(array) {
 
 async function getAverageTime(repeatTimes) {
 
+    const numbers = JSON.parse(numbersJson);
+    qsort(numbers);
+
     const measures = [];
     for (let i = 0; i < repeatTimes; i++) {
         const numbers = JSON.parse(numbersJson);
@@ -69,13 +72,9 @@ async function getAverageTime(repeatTimes) {
     return measures.at(Math.floor(measures.length / 2));
 }
 
-document.getElementById('result').innerText = (`Speed: ${await getAverageTime(10)} ms`);
-
-
-// Generate a random array of numbers
-// const numbers = [];
-// for (let i = 0; i < 1e7; i++) {
-//     numbers.push(Math.floor(Math.random() * 1e8));
-// }
-
-// console.log('New arr: ', numbers);
+try {
+    document.getElementById('result').innerText = (`Speed: ${await getAverageTime(10)} ms`);
+} catch (err) {
+    document.getElementById('result').innerText = (`Error. Go to console to get err msg.`);
+    throw err;
+}
